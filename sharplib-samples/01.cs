dab3fb2e073a88b0120842208b4751509d2818b5
Gordon Beeming <gpg@gordonbeeming.com>
2024-01-16 21:00:20 +1000

    adding sample code

---
+++ sharplib-samples/01.cs
@@ -0,0 +1,28 @@
+
+using System;
+// https://sharplab.io/#v2:EYLgxg9gTgpgtADwGwBYA+ABATARgLABQGAzAATbk4DsphA3oaU+WRiqQLICGAlgHYAKAJS0CzUgzHjmANy5RS8gOakAvKQCcAbkbSmAZwDuPAC5gAFgOVDdeyXr1gu+mKRwAGELYd6MODVZQSkI6Uj5MwLBcANah4cxOLqRYnt7xTH4B1nHpkTAxOfGJrsSpYekZ/oHBheF5BWkOxaQoZRXimdUhjXr1sT3SzQCsbe2VWUHd5T59teIAvt6LBPNAA==
+public class Program
+{
+    public void Main()
+    {
+        var arg = 9;
+        switch(arg)
+        {
+            case 10:
+                Console.WriteLine(arg);
+            break;
+            case 20:
+                Console.WriteLine(arg);
+            break;
+            case 30:
+                Console.WriteLine(arg);
+            break;
+            case 40:
+                Console.WriteLine(arg);
+            break;
+            case 50:
+                Console.WriteLine(arg);
+            break;
+        }
+    }
+}
